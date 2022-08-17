e7a7496bf4d22c8bcd338b88cf2daab708194c7e
prasathram387 <prasathram387@gmail.com>
2022-08-17 14:58:21 +0530

    added some variables

--- Print.java
+++ Print.java
@@ -2,5 +2,8 @@ public class Print {
     public void main(String args[]) {
         System.out.println("Hello world ");
         System.out.println("Hi");
+	int a = 10;
+	int b = 2;
+	int c = a + b;
     }
 }
